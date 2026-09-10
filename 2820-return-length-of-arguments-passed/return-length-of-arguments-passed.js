5941225911276f527c15f52c8fe18d7a9dfdd32c
Rounok Paul <157970186+ROUNOKPAUL@users.noreply.github.com>
2026-09-11 02:46:02 +0530

    Time: 38 ms (80.61%) | Memory: 54.1 MB (14.51%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-    let count=0;
+    return args.length
+    /**let count=0;
     for(let i=0;i<args.length;i++){
         count+=1
     }
-    return count
+    return count**/
 };
 
 /**
